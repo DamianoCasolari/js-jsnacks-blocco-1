7c88f14eb805d8938be36fc19e49604800388927
DamianoCasolari <damianocasolari@gmail.com>
2023-03-14 12:22:09 +0100

    fix lenght with length

--- jssnack2/script.js
+++ jssnack2/script.js
@@ -1,14 +1,15 @@
-const user_number_1 = prompt("Inserisci la prima parola")
-const user_number_2 = prompt("Inserisci la seconda parola")
+const user_word_1 = prompt("Inserisci la prima parola")
+const user_word_2 = prompt("Inserisci la seconda parola")
+
 
 
 const result = document.querySelector("h1")
 
-if (user_number_1.length > user_number_2.lenght) {
-    result.innerHTML = `La prima parola ${user_number_1} è più lunga di ${user_number_2} `
-} else if (user_number_1.length < user_number_2.length) {
-    result.innerHTML = `La seconda parola ${user_number_2} è più lunga della prima ${user_number_1}`
+if (user_word_1.length > user_word_2.length) {
+    result.innerHTML = `La prima parola ${user_word_1} è più lunga di ${user_word_2} `
+} else if (user_word_1.length < user_word_2.length) {
+    result.innerHTML = `La seconda parola ${user_word_2} è più lunga della prima ${user_word_1}`
 } else {
-    result.innerHTML = `Le parole corrispondo allo stesso numero di lettere `
+    result.innerHTML = `Le parole ${user_word_2} e ${user_word_1} corrispondo allo stesso numero di lettere`
 
 }
