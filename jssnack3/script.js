763f744ea62bbac22431afb17593c92fa48aaf94
DamianoCasolari <damianocasolari@gmail.com>
2023-03-15 11:48:33 +0100

    snack3 pt2

--- jssnack3/script.js
+++ jssnack3/script.js
@@ -1,12 +1,28 @@
 
 
 
+// const result = document.querySelector("h1")
+// let totale = 0;
+
+// for (let i = 0; i < 10; i++) {
+//     const user_current_number = Number(prompt("Inserisci un numero"));
+//     totale += user_current_number;
+// }
+
+// result.innerHTML = `il totale dei 10 numeri inseriti corrisponde a ${totale}`
+
+
+
+// con ciclo While 
+
 const result = document.querySelector("h1")
 let totale = 0;
 
-for (let i = 0; i < 10; i++) {
+let i = 0;
+while (i < 10) {
     const user_current_number = Number(prompt("Inserisci un numero"));
     totale += user_current_number;
+    i++
 }
 
 result.innerHTML = `il totale dei 10 numeri inseriti corrisponde a ${totale}`
